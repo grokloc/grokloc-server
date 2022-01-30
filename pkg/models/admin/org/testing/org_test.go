aa6b32c84f5ca29b79a181760d24967002b48518
Brad Clawsie <brad@b7j0c.org>
2022-01-30 12:20:33 -0600

    org owner update tests

--- pkg/models/admin/org/testing/org_test.go
+++ pkg/models/admin/org/testing/org_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/grokloc/grokloc-server/pkg/env"
 	"github.com/grokloc/grokloc-server/pkg/models"
 	"github.com/grokloc/grokloc-server/pkg/models/admin/org"
+	"github.com/grokloc/grokloc-server/pkg/models/admin/user"
 	"github.com/grokloc/grokloc-server/pkg/security"
 	"github.com/grokloc/grokloc-server/pkg/state"
 	"github.com/stretchr/testify/require"
@@ -71,6 +72,122 @@ func (s *OrgSuite) TestUpdateStatus() {
 	o_read, err := org.Read(context.Background(), o.ID, s.st.RandomReplica())
 	require.Nil(s.T(), err)
 	require.Equal(s.T(), models.StatusInactive, o_read.Meta.Status)
+}
+
+func (s *OrgSuite) TestUpdateOwner() {
+	ownerPassword, err := security.DerivePassword(uuid.NewString(), s.st.Argon2Cfg)
+	require.Nil(s.T(), err)
+
+	o, err := org.Create(
+		context.Background(),
+		uuid.NewString(), // org name
+		uuid.NewString(), // org owner display name
+		uuid.NewString(), // org owner email
+		ownerPassword,
+		s.st.DBKey,
+		s.st.Master,
+	)
+	require.Nil(s.T(), err)
+
+	newOwnerPassword, err := security.DerivePassword(uuid.NewString(), s.st.Argon2Cfg)
+	require.Nil(s.T(), err)
+
+	newOwner, err := user.Create(
+		context.Background(),
+		uuid.NewString(), // display name
+		uuid.NewString(), // email
+		o.ID,
+		newOwnerPassword,
+		s.st.DBKey,
+		s.st.Master,
+	)
+	require.Nil(s.T(), err)
+
+	// will fail - user is still unconfirmed
+	err = o.UpdateOwner(
+		context.Background(),
+		newOwner.ID,
+		s.st.Master,
+	)
+	require.NotNil(s.T(), err)
+	require.Equal(s.T(), models.ErrRelatedUser, err)
+
+	err = newOwner.UpdateStatus(
+		context.Background(),
+		models.StatusActive,
+		s.st.Master,
+	)
+	require.Nil(s.T(), err)
+
+	// new owner is now active
+	err = o.UpdateOwner(
+		context.Background(),
+		newOwner.ID,
+		s.st.Master,
+	)
+	require.Nil(s.T(), err)
+	require.Equal(s.T(), o.Owner, newOwner.ID)
+}
+
+func (s *OrgSuite) TestUpdateOwnerWrongOrg() {
+	ownerPassword, err := security.DerivePassword(uuid.NewString(), s.st.Argon2Cfg)
+	require.Nil(s.T(), err)
+
+	o, err := org.Create(
+		context.Background(),
+		uuid.NewString(), // org name
+		uuid.NewString(), // org owner display name
+		uuid.NewString(), // org owner email
+		ownerPassword,
+		s.st.DBKey,
+		s.st.Master,
+	)
+	require.Nil(s.T(), err)
+
+	oOther, err := org.Create(
+		context.Background(),
+		uuid.NewString(), // org name
+		uuid.NewString(), // org owner display name
+		uuid.NewString(), // org owner email
+		ownerPassword,
+		s.st.DBKey,
+		s.st.Master,
+	)
+	require.Nil(s.T(), err)
+
+	// cannot make the owner of o the owner of oOther
+	err = oOther.UpdateOwner(
+		context.Background(),
+		o.Owner,
+		s.st.Master,
+	)
+	require.NotNil(s.T(), err)
+	require.Equal(s.T(), models.ErrRelatedUser, err)
+}
+
+func (s *OrgSuite) TestUpdateOwnerMissing() {
+	ownerPassword, err := security.DerivePassword(uuid.NewString(), s.st.Argon2Cfg)
+	require.Nil(s.T(), err)
+
+	o, err := org.Create(
+		context.Background(),
+		uuid.NewString(), // org name
+		uuid.NewString(), // org owner display name
+		uuid.NewString(), // org owner email
+		ownerPassword,
+		s.st.DBKey,
+		s.st.Master,
+	)
+	require.Nil(s.T(), err)
+
+	// prospective new owner doesn't exist
+	err = o.UpdateOwner(
+		context.Background(),
+		uuid.NewString(),
+		s.st.Master,
+	)
+	require.NotNil(s.T(), err)
+	require.Equal(s.T(), models.ErrRelatedUser, err)
 }
 
 func TestOrgSuite(t *testing.T) {
